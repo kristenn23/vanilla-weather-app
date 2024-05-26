f80a741edb4546da87a07bb13295800681fa5d20
kristenn23 <nazarethkristen@gmail.com>
2024-05-26 16:16:56 -0400

    added more styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   console.log(searchInput.value);
-  let city = searchInput.value;
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
@@ -11,12 +10,15 @@ searchFormElement.addEventListener("submit", handleSearchSubmit);
 function updateWeather(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
+
 function searchCity(city) {
   let apiKey = "b187a116ta5dc648f1o84f8b1a5e4430";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=b187a116ta5dc648f1o84f8b1a5e4430&units=metric`;
   axios.get(apiUrl).then(updateWeather);
 }
+searchCity("Portugal");
